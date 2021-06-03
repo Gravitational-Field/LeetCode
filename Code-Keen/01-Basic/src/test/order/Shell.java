e48d48dcba97fd81e3e7f90387251c096468c8bc
Keen <lizhangjie316@163.com>
2021-06-03 09:31:14 +0800

    希尔排序

---
+++ Code-Keen/01-Basic/src/test/order/Shell.java
@@ -0,0 +1,29 @@
+package test.order;
+
+/**
+ * @ClassName Shell
+ * @Description: TODO
+ * @Author Keen
+ * @DATE 2021/6/1 10:05
+ * @Version 1.0
+ **/
+public class Shell<T extends Comparable<T>> extends Sort<T>  {
+    @Override
+    public void sort(T[] nums) {
+        int N = nums.length;
+        int h = 1;
+
+        while (h < N / 3) { //确定初始的h
+            h = 3 * h + 1; // 1, 4, 13, 40, ...
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < N; i++) { //从此往后
+                for (int j = i; j >= h && less(nums[j], nums[j - h]); j -= h) {  //每次都往前比
+                    swap(nums, j, j - h);
+                }
+            }
+            h = h / 3; //比完一轮，更新h值
+        }
+    }
+}
