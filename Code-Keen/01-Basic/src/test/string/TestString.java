ea2991ccf00546827e41c7a41c79e61cb8761cbb
Keen <lizhangjie316@163.com>
2021-06-16 19:14:22 +0800

    testString

---
+++ Code-Keen/01-Basic/src/test/string/TestString.java
@@ -0,0 +1,32 @@
+package test.string;
+
+import org.junit.Test;
+
+/**
+ * @ClassName TestString
+ * @Description: TODO
+ * @Author Keen
+ * @DATE 2021/6/13 19:50
+ * @Version 1.0
+ **/
+
+
+public class TestString {
+
+    @Test
+    public void testString() {
+
+    }
+
+
+    @Test
+    public void testStringBuilder() {
+        StringBuilder sb = new StringBuilder();
+    }
+
+    @Test
+    public void testStringBuffer() {
+        StringBuffer stringBuffer = new StringBuffer();
+        RuntimeException runtimeException = new RuntimeException();
+    }
+}
